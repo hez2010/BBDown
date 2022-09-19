37b476e0b3e1234c44bcf9a0149ee3c96604fb6d
Steven He <hez2010@outlook.com>
2022-09-19 17:13:13 +0900

    Initial UI

---
+++ BBDown.Ava/MainWindow.cs
@@ -0,0 +1,13 @@
+
+using Avalonia.Controls;
+
+namespace BBDown.Ava;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        textBox1.Text = "hello";
+    }
+}
